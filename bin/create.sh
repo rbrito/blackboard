e681411ff1e9af6930be61e9d1932ea6f1838a90
Rogério Brito <rbrito@ime.usp.br>
2011-10-08 11:49:19 -0300

    Improve the creation script.

--- bin/create.sh
+++ bin/create.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# If necessary, we can creat map/{dvips,dvipdfm,pdftex}/updmap/dejavubb
-mkdir -p texmf/fonts/{{afm,tfm,source,type1}/public/dejavubb,map/dvips/updmap/dejavubb}
-cp dejavubb.map			texmf/fonts/map/dvips/updmap/dejavubb/
-cp ../DejaVuSans.sfd		texmf/fonts/source/public/dejavubb/
-cp DejaVuBB.afm			texmf/fonts/afm/public/dejavubb/
-cp DejaVuBB.pfb			texmf/fonts/type1/public/dejavubb/
-cp DejaVuBB.pfm			texmf/fonts/type1/public/dejavubb/
-cp DejaVuBB.tfm			texmf/fonts/tfm/public/dejavubb/
+FNTNAME=dejavubb
+
+# If necessary, we can creat map/{dvips,dvipdfm,pdftex}/updmap/$FNTNAME
+mkdir -p texmf/fonts/{{afm,tfm,type1}/public/$FNTNAME,map/dvips/$FNTNAME}
+# cp sfd/DejaVuSans.sfd			texmf/fonts/source/public/$FNTNAME/
+
+cp map/$FNTNAME.map		texmf/fonts/map/dvips/$FNTNAME/
+cp type1/$FNTNAME.afm		texmf/fonts/afm/public/$FNTNAME/
+cp type1/$FNTNAME.pf{b,m}	texmf/fonts/type1/public/$FNTNAME/
+cp type1/$FNTNAME.tfm		texmf/fonts/tfm/public/$FNTNAME/
